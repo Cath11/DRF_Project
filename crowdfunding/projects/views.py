73f499566822a57bd19abf62e202c94c92692703
nicoletteheath <67173671+nicoletteheath@users.noreply.github.com>
2020-08-22 11:17:45 +0800

    saturday class - adding permissions

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,11 +1,15 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+from .permissions import IsOwnerOrReadOnly
 
 class ProjectList(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+        ]
 
     def get(self, request):
         projects = Project.objects.all()
@@ -26,6 +30,10 @@ class ProjectList(APIView):
         )    
 
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+        ]
 
     def get_object(self, pk):
         try:
@@ -37,6 +45,17 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
 
 class PledgeList(APIView):
 
